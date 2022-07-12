ece8cfc0ed981dee6aee7ed806b1e75a3ff1c8d4
KinkHoang <viethoangdn.it@gmail.com>
2022-07-12 16:11:01 +0700

    Remove sign un used messenger config

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
     <div>
       <Router />
       <MessengerCustomerChat
-    pageId="<109134945196808>"
-    appId="<737329090719652>"
-    htmlRef="<REF_STRING>"
+    pageId="109134945196808"
+    appId="737329090719652"
   />,
     </div>
   );
